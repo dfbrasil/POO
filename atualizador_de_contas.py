c1744e9a8a4c9859c93899ae8df0717c33f8c818
dfbrasil <danieldfb@gmail.comm>
2022-11-25 16:34:03 -0300

    123

--- atualizador_de_contas.py
+++ atualizador_de_contas.py
@@ -1,10 +1,13 @@
-from heranca import Conta, ContaPoupanca, ContaCorrente, Cliente
+from banco1 import Banco
+from heranca import Cliente, Conta, ContaCorrente, ContaPoupanca
+
 
 class AtualizadorDeContas:
 
-    def __init__(self, selic, saldo_total):
+    def __init__(self, selic, saldo_total, dicio):
         self.selic = selic
         self.saldo_total = saldo_total
+        self.dicio = dicio
 
     def roda(self, conta):
         print (f'Saldo da Conta: {conta.saldo}')
@@ -12,18 +15,18 @@ class AtualizadorDeContas:
         print(f'Saldo Final: {self.saldo_total}')
 
 if __name__ == '__main__':
-    c = Conta('123-4', 'Joao', 1000.0)
-    cc = ContaCorrente('123-5', 'Jose', 1000.0)
-    cp = ContaPoupanca('123-6', 'Maria', 1000.0)
+    # c = Conta('123-4', 'Joao', 1000.0)
+    # cc = ContaCorrente('123-5', 'Jose', 1000.0)
+    # cp = ContaPoupanca('123-6', 'Maria', 1000.0)
 
-    adc = AtualizadorDeContas(0.01, 1000)
+    # adc = AtualizadorDeContas(0.01, 1000)
 
-    adc.roda(c)
-    adc.roda(cc)
-    adc.roda(cp)
+    # adc.roda(c)
+    # adc.roda(cc)
+    # adc.roda(cp)
 
-    print(f'Saldo total: {adc.saldo_total}')
+    # print(f'Saldo total: {adc.saldo_total}')
+    b1 = Banco('numero', 'titular', 'saldo')
 
-
-
-    
+    print(b1.adiciona())
+   
